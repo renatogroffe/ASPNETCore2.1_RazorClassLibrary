1642d062a469c32f7b9c4287ff1345bd9b56f867
Renato Groffe <renato.groffe@yahoo.com.br>
2018-07-16 01:54:24 -0300

    ASP.NET Core 2.1 + Razor Class Libary
    ASP.NET Core 2.1 + Razor Class Libary

---
+++ ExemplosRazorClassLibrary/LibAmbiente/Areas/Sistema/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibAmbiente.Sistema.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            TempData["MachineName"] = Environment.MachineName;
+            TempData["Sistema"] = Environment.OSVersion.VersionString;
+        }
+    }
+}
